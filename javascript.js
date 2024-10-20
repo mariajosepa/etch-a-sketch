0782bff3e6eb36ed53de991241cabc253bfb9bf6
mariajose <mariamariapava123@gmail.com>
2024-10-20 09:33:51 -0500

    [NEW] added rainbow mode and darken mode. However, their behavior is not yet finished.

--- javascript.js
+++ javascript.js
@@ -2,7 +2,19 @@
 grid = document.querySelector(".grid");
 dimension = document.getElementById('dimension');
 update = document.getElementById('update');
+rainbow = document.getElementById('rainbow');
+darken = document.getElementById('darken');
+selection = document.querySelector('.selection');
 
+
+const generateRandomColor = () => {
+
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  return `rgb(${r}, ${g}, ${b})`;  
+}
 
 document.addEventListener("DOMContentLoaded",() =>{
 
@@ -12,16 +24,38 @@ document.addEventListener("DOMContentLoaded",() =>{
     grid.appendChild(gridItem);
   }
 
-  gridElements = document.getElementsByClassName("grid-item");
-
-  for (let i = 0; i < gridElements.length; i++) {
-    gridElements[i].addEventListener("mouseover",(event)=>{
-        event.target.style.backgroundColor = "black";
-    });
-    
-}
-
 })
+
+//we use this to change the color of hovered over
+grid.addEventListener("mouseover",(event)=>{
+
+  if (event.target.classList.contains("grid-item")){
+
+
+    if (rainbow.classList.contains('clicked')){
+      event.target.style.opacity = "1.0";
+      event.target.style.backgroundColor = generateRandomColor();
+      event.target.classList.add('painted');
+    }else{
+      event.target.style.backgroundColor = "grey";
+      event.target.classList.add('painted');
+    }
+
+    if (darken.classList.contains('clicked')) {
+      // Get current opacity
+      let currentOpacity = parseFloat(getComputedStyle(event.target).opacity);
+
+      // Decrease opacity by 10%, ensuring it doesn't go below 0
+      currentOpacity = Math.max(0, currentOpacity - 0.1); // Ensure opacity doesn't go below 0
+
+      // Apply the updated opacity
+      event.target.style.opacity = currentOpacity;
+    }
+  
+
+     
+  }
+});
 
 const changeGrid = (dimension) => {
 
@@ -32,10 +66,6 @@ const changeGrid = (dimension) => {
     for (let i = 0; i < numChildren; i++) {
         const gridItem = document.createElement("div");
         gridItem.classList.add("grid-item");
-
-        gridItem.addEventListener("mouseover",(event)=>{
-            event.target.style.backgroundColor = "black";
-        });
         grid.appendChild(gridItem);        
         gridItem.style.flex = `1 0 calc(100% / ${dimension} - 1.5px)`;
       }
@@ -50,4 +80,13 @@ update.addEventListener("click", () => {
         changeGrid(dimension.value);
     }
 });
+
+selection.addEventListener("click", (event) =>{
+    if (event.target.id === "rainbow" || event.target.id === "darken" || event.target.id === "update"){
+        event.target.classList.toggle("clicked");
+    }
+});
+
+
+
 
